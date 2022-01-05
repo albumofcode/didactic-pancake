667e0e6277949ad3291c57a4684b018a30895745
albumofcode <97133130+albumofcode@users.noreply.github.com>
2022-01-04 20:48:56 -0500

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace didacticpancake
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.txtFirstNum = new System.Windows.Forms.TextBox();
+            this.txtSecondNum = new System.Windows.Forms.TextBox();
+            this.lblComputedNum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(259, 257);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 36);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Location = new System.Drawing.Point(401, 257);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(100, 36);
+            this.btnSubtract.TabIndex = 1;
+            this.btnSubtract.Text = "Subtract";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // txtFirstNum
+            // 
+            this.txtFirstNum.Location = new System.Drawing.Point(259, 175);
+            this.txtFirstNum.Name = "txtFirstNum";
+            this.txtFirstNum.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstNum.TabIndex = 2;
+            // 
+            // txtSecondNum
+            // 
+            this.txtSecondNum.Location = new System.Drawing.Point(401, 175);
+            this.txtSecondNum.Name = "txtSecondNum";
+            this.txtSecondNum.Size = new System.Drawing.Size(100, 20);
+            this.txtSecondNum.TabIndex = 3;
+            // 
+            // lblComputedNum
+            // 
+            this.lblComputedNum.AutoSize = true;
+            this.lblComputedNum.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComputedNum.Location = new System.Drawing.Point(477, 103);
+            this.lblComputedNum.Name = "lblComputedNum";
+            this.lblComputedNum.Size = new System.Drawing.Size(24, 23);
+            this.lblComputedNum.TabIndex = 4;
+            this.lblComputedNum.Text = "0";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblComputedNum);
+            this.Controls.Add(this.txtSecondNum);
+            this.Controls.Add(this.txtFirstNum);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "MainForm";
+            this.Text = "Simple Calculator";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.TextBox txtFirstNum;
+        private System.Windows.Forms.TextBox txtSecondNum;
+        private System.Windows.Forms.Label lblComputedNum;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,72 @@
+﻿//Wrote this in 15min for my friends final project during freshman year. Devious, but she got an A!
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace didacticpancake
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            txtFirstNum.Text = "Enter a number";
+            txtSecondNum.Text = "Enter a number";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtFirstNum.Text, out double dblFirstNum) || !double.TryParse(txtSecondNum.Text, out double dblSecondNum))
+            {
+                txtFirstNum.Text = "You must enter a number";
+                txtSecondNum.Text = "You must enter a numer";
+            }
+
+            else
+            {
+                lblComputedNum.Text = Convert.ToString(ComputeAddition(dblFirstNum, dblSecondNum));
+            }
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+
+            if (!double.TryParse(txtFirstNum.Text, out double dblFirstNum) || !double.TryParse(txtSecondNum.Text, out double dblSecondNum))
+            {
+                txtFirstNum.Text = "You must enter a number";
+                txtSecondNum.Text = "You must enter a number";
+            }
+
+            else
+            {
+                lblComputedNum.Text = Convert.ToString(ComputeSubtraction(dblFirstNum, dblSecondNum));
+            }
+        }
+
+        public double ComputeAddition(double dblNum1, double dblNum2)
+        {
+            double compute = dblNum1 + dblNum2;
+
+            return compute;
+        }
+
+        public double ComputeSubtraction(double dblNum1, double dblNum2)
+        {
+            double compute = dblNum1 - dblNum2;
+
+            return compute;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
